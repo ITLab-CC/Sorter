ec811306531f87e922c585462fe5f9c81012c2a4
JulianKropp <62835694+JulianKropp@users.noreply.github.com>
2026-04-21 11:36:39 +0200

    📝 Comment out code for saving all frames

--- sensor/camera.py
+++ sensor/camera.py
@@ -184,4 +184,12 @@ if __name__ == "__main__":
         cv2.imwrite("/out/last_frame.png", last_frame_rgb)
         print("Saved first_frame.png and last_frame.png.")
 
+    # # svae first and last frames for visual confirmation
+    # i = 0
+    # for frame in video_frames:
+    #     i += 1
+    #     first_frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BAYER_RG2RGB)
+    #     cv2.imwrite(f"./out/{i}_frame.png", first_frame_rgb)
+    #     print("Saved first_frame.png and last_frame.png.")
+
     cam.release_camera()
