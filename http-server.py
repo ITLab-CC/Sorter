fb22678df2d5a210e8948186898111da0f1d57d8
JulianKropp <62835694+JulianKropp@users.noreply.github.com>
2026-05-05 09:23:10 +0200

    🔒️ set dataset directory for HTTP server

--- http-server.py
+++ http-server.py
@@ -1,9 +1,14 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from functools import partial
+from pathlib import Path
+import os
 
 class CORSRequestHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
         super().end_headers()
 
-# Serves on port 1000
+dataset_dir = Path(__file__).parent / "dataset"
+os.chdir(dataset_dir)
+
 HTTPServer(('0.0.0.0', 1000), CORSRequestHandler).serve_forever()
