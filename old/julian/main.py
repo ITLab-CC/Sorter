ad39cc5188ea530cefa0c171c76cdec24b41481b
JulianKropp <62835694+JulianKropp@users.noreply.github.com>
2026-06-09 14:23:06 +0200

    ⚰️

--- old/julian/main.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-
-# Imshow durch Matplotlib ersetzen
-if image is not None:
-    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    plt.axis('off')  # Achsen entfernen
-    plt.show()
